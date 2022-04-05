e9b4df79640b317f834a4897ae1c4643007b9c58
Janka K <janakorycanova@gmail.com>
2022-04-05 18:32:05 +0200

    Obtain data url attribute from  body element

--- ovoce.js
+++ ovoce.js
@@ -1,3 +1,8 @@
 let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
 let druheOvoce = document.querySelector('#muj-druhy-div').dataset.ovoce;
 console.log(prvniOvoce,druheOvoce);
+
+let dataUrl= document.querySelector('body').dataset.url;
+
+console.log(dataUrl);
+
